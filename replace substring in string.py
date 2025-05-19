5f5af2adc9285dc80df9afcba2e8d56b6d753339
Tahirshaikh-0ss <tahirshaikhqwee@gmail.com>
2025-05-19 12:33:14 -0700

    Add files via upload

---
+++ replace substring in string.py
@@ -0,0 +1,7 @@
+
+text = input("Enter the main string: ")
+old_sub = input("Enter the substring to replace: ")
+new_sub = input("Enter the new substring: ")
+
+result = text.replace(old_sub, new_sub)
+print("Updated string:", result)
