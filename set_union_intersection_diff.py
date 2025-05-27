dedc352d4b1797ce436ba5cbaaa614f756413a3a
snehalunde09 <snehalunde2005@gmail.com>
2025-05-27 09:51:59 +0530

    day-4

---
+++ set_union_intersection_diff.py
@@ -0,0 +1,8 @@
+a={1,2,3}
+b={6,3,7}
+union=a.union(b)
+intersection=a.intersection(b)
+difference=a.difference(b)
+print("union :",union)
+print("intersectuon :", intersection)
+print("difference :", difference)
